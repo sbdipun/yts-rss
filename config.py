17a20170fd61bd99b1c6ad691aec92ff61b75755
sbdipun <87560288+sbdipun@users.noreply.github.com>
2025-05-14 21:45:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 
 # Environment-based Config
-TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-TELEGRAM_CHANNEL_ID = os.getenv("TELEGRAM_CHANNEL_ID")
+TELEGRAM_BOT_TOKEN = os.getenv("7607848214:AAH-FURy_kD1Vybj5WvHb_hseWqnF8QE83k")
+TELEGRAM_CHANNEL_ID = os.getenv("-1001651019056")
 
 # Optional values
 RSS_URL = os.getenv("RSS_URL", "https://yts.mx/rss/0/all/all/0/all ")
-CHECK_INTERVAL_MINUTES = int(os.getenv("CHECK_INTERVAL_MINUTES", "5"))
+CHECK_INTERVAL_MINUTES = int(os.getenv("CHECK_INTERVAL_MINUTES", "2"))
