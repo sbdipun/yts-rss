2173e2b9003cd72bc98986cd6c0a90d7794d4bd2
sbdipun <87560288+sbdipun@users.noreply.github.com>
2025-05-14 22:18:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 # Environment-based Config
-TELEGRAM_BOT_TOKEN = os.getenv("7607848214:AAH-FURy_kD1Vybj5WvHb_hseWqnF8QE83k")
-TELEGRAM_CHANNEL_ID = os.getenv("-1001651019056")
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")     # ← This is correct
+TELEGRAM_CHANNEL_ID = os.getenv("TELEGRAM_CHANNEL_ID")   # ← This is correct
 
 # Optional values
 RSS_URL = os.getenv("RSS_URL", "https://yts.mx/rss/0/all/all/0/all ")
