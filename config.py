820e312d2031bb29bf31592a9f27caa058fc0107
sbdipun <87560288+sbdipun@users.noreply.github.com>
2025-05-14 21:43:43 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+# Environment-based Config
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+TELEGRAM_CHANNEL_ID = os.getenv("TELEGRAM_CHANNEL_ID")
+
+# Optional values
+RSS_URL = os.getenv("RSS_URL", "https://yts.mx/rss/0/all/all/0/all ")
+CHECK_INTERVAL_MINUTES = int(os.getenv("CHECK_INTERVAL_MINUTES", "5"))
