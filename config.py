b9c17442e8c8d827ad057041963537289a4320c6
sbdipun <87560288+sbdipun@users.noreply.github.com>
2025-07-16 00:29:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ TELEGRAM_CHANNEL_ID = os.getenv("TELEGRAM_CHANNEL_ID")
 
 # Optional values
 # config.py
-TORRENTING_RSS_URL = os.getenv("TORRENTING_RSS_URL", "https://torrenting.com/t.rss?5;18;4;82;49;99;47;38;11;55;3;40;1;u=1767736;tp=bnpjvcnhvnxqihbcxbokpcojzopxdgnt;DaddyCooL;private;do-not-share")
+
 YTS_RSS_URL = os.getenv("RSS_URL", "https://yts.mx/rss/0/all/all/0/all")
 CHECK_INTERVAL_MINUTES = int(os.getenv("CHECK_INTERVAL_MINUTES", "2"))
 HDENCODE_RSS_URL = os.getenv("HDENCODE_RSS_URL", "https://hdencode.org/feed/?sfw=pass1747284999")
@@ -20,3 +20,4 @@ BWTORRENTS_RSS_URL = os.getenv(
     "BWTORRENTS_RSS_URL",
     " https://bwtorrents.cc/rss.php?cat=145,120,188,125,116,124,114,117,189,190,113,118,115,121,119,194,192,128,132,129,130,127,131,141,140&passkey=22e2c513c1f2f48b8e9c126f537c9903"
 )
+TORRENTING_RSS_URL = os.getenv("TORRENTING_RSS_URL", "https://torrenting.com/t.rss?5;18;4;82;49;99;47;38;11;55;3;40;1;u=1767736;tp=bnpjvcnhvnxqihbcxbokpcojzopxdgnt;DaddyCooL;private;do-not-share")
