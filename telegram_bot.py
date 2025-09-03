36ac2ee5e81826fde7a74935cf1d61eb705711c6
sbdipun <87560288+sbdipun@users.noreply.github.com>
2025-09-03 11:17:54 +0530

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -53,6 +53,12 @@ def post_to_telegram(bot_token, chat_id, items):
                 f"📦 Size: {item['size']}\n\n"
                 f"<b>{item['torrent_link']}</b>"
             )
+        elif source == 'sharespark':
+            message = (
+                f"📀 <b>{item['title']}</b>\n"
+                f"<b>Category: {item['category']}</b>\n\n"
+                f"<b>{item['torrent_link']}</b>"
+            )
         else:  # tbl or unknown
             message = (
                 f"📀 <b>{item['title']}</b>\n\n"
